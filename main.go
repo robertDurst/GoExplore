88356904cf76c5fcda08fb71a1093063ebf0781c
robertDurst <me@robdurst.com>
2022-01-19 13:39:27 -0500

    improve parser

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"unicode"
 )
 
+type SExpression interface {
+	GetName()
+}
+
 /*
 	The most elementary type of S-Expression is the atomic symbol.
 
@@ -22,50 +26,92 @@ import (
 		EXTRALONGSTRINGOFLETTERS
 		A4B66XYZ2
 */
-func parseAtom(word string) {
-	fmt.Println(word)
+type Atom struct {
+	name string
 }
 
-func parse(line string) {
+func (a Atom) GetName() {
+	fmt.Printf("Atom: %s\n", a.name)
+}
+
+type Dot struct{}
+
+func (d Dot) GetName() {
+	fmt.Println("Dot")
+}
+
+type LParen struct{}
+
+func (l LParen) GetName() {
+	fmt.Println("Left Paren")
+}
+
+type RParen struct{}
+
+func (r RParen) GetName() {
+	fmt.Println("Right Paren")
+}
+
+func parse(line string) []SExpression {
+	tokens := make([]SExpression, 0)
+
 	words := strings.Split(line, " ")
 
 	// split by word
 	for _, word := range words {
-		isWord := false
-
-		wordAsRune := []rune(word)
-		if unicode.IsLetter(wordAsRune[0]) {
-			isWord = true
-		}
-
 		var buffer bytes.Buffer
-		if isWord {
-			buffer.WriteRune(wordAsRune[0])
-		}
+		isWord := false
+		wordAsRune := []rune(word)
 
 		// split by character of word
 		for i, char := range strings.Split(word, "") {
-			if isWord && i == 0 {
+			if !isWord && unicode.IsLetter(wordAsRune[i]) {
+				isWord = true
+				buffer.WriteRune(wordAsRune[i])
 				continue
 			}
 
 			if char == ")" {
-				break
+				if isWord {
+					isWord = false
+					tokens = append(tokens, Atom{name: buffer.String()})
+					buffer.Reset()
+				}
+				tokens = append(tokens, RParen{})
 			} else if char == "(" {
-				break
+				if isWord {
+					isWord = false
+					tokens = append(tokens, Atom{name: buffer.String()})
+					buffer.Reset()
+				}
+				tokens = append(tokens, LParen{})
 			} else if char == "." {
-				break
+				if isWord {
+					isWord = false
+					tokens = append(tokens, Atom{name: buffer.String()})
+					buffer.Reset()
+				}
+				tokens = append(tokens, Dot{})
 			} else if unicode.IsNumber(wordAsRune[i]) || unicode.IsLetter(wordAsRune[i]) {
 				buffer.WriteRune(wordAsRune[i])
 			}
 		}
 
-		fmt.Println(buffer.String())
+		// in case we;re still in a word after finishing iteration here
+		if isWord {
+			tokens = append(tokens, Atom{name: buffer.String()})
+		}
 	}
+
+	return tokens
 }
 
 func interpretLine(line string) {
-	parse(line)
+	tokens := parse(line)
+
+	for _, token := range tokens {
+		token.GetName()
+	}
 }
 
 func getNextInput() string {
